7d277c9301b41c7de769a51f7762ccaebecee01f
Hami Berkay Aktaş <hberkayaktas@gmail.com>
2022-06-28 02:56:02 +0300

    footer updated

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 mt-4 border-top">
-      <p className="col-md-4 mb-0 text-muted">Berkay aktaş Tarafından 2022 yılında </p>
+      <p className="col-md-4 mb-0 text-muted">Berkay aktaş Tarafından 2022 yılında yapılmıştır</p>
       <ul className="nav col-md-4 justify-content-end">
         <li className="nav-item">
           <a href="https://github.com/hberkayaktas" className="nav-link px-2 text-muted"><i class="fa-brands fa-github"></i></a>
